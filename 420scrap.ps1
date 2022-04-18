85c0c2646c57ebf785ba57c8a9d238f510ee41c3
Syl S <SSyl@users.noreply.github.com>
2022-04-18 13:57:57 -0500

    Fix if statement so it actually works. Import assemblies rather than do it by reflection.

--- 420scrap.ps1
+++ 420scrap.ps1
@@ -1,3 +1,13 @@
+#Checks if Rust is running. If not, then it exits the script.
+if( -not (Get-Process -Name RustClient -ErrorAction SilentlyContinue))
+{
+    exit;
+}
+
+# Import assemblies for later
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -Assembly "Microsoft.VisualBasic"
+
 $scrapTime = Get-Date "4-1-2001 3:50 AM"; # Initial time that there was a scrap drop in the past.
 $timeNow = [System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId([DateTime]::now, "Central Standard Time"); # Get the current time in the US Central Time Zone. It has to be in this timezone to work.
 
@@ -24,15 +34,13 @@ else {
     Set-Clipboard -Value "chat.say `"420 scrap drops in ${minutes}m at $scrapTime (Central Time)`"";
 }
 
-[void] [System.Reflection.Assembly]::LoadWithPartialName("'Microsoft.VisualBasic")
-if ([Microsoft.VisualBasic.Interaction]::AppActivate("Rust")) {
-    [void] [System.Reflection.Assembly]::LoadWithPartialName("'System.Windows.Forms")
-    [System.Windows.Forms.SendKeys]::SendWait("{F1}");
-    Start-Sleep -Milliseconds 50 # It takes a few miliseconds to actually grab the content from the clipboard. Without this, it fails at pasting. Might need to increase the value depending on system performance.
-    [System.Windows.Forms.SendKeys]::SendWait("^v");
-    [System.Windows.Forms.SendKeys]::SendWait("~");
-    [System.Windows.Forms.SendKeys]::SendWait("{F1}");
-}
+[Microsoft.VisualBasic.Interaction]::AppActivate("Rust")
+[System.Windows.Forms.SendKeys]::SendWait("{F1}");
+Start-Sleep -Milliseconds 10 # It takes a few miliseconds to actually grab the content from the clipboard. Without this, it fails at pasting. Might need to increase the value depending on system performance.
+[System.Windows.Forms.SendKeys]::SendWait("^v");
+[System.Windows.Forms.SendKeys]::SendWait("~");
+[System.Windows.Forms.SendKeys]::SendWait("{F1}");
+
 
 # The above uses VisualBasic and the built in System.Windows.Forms assemblies, but it's also possible to do this using wscript.shell. It's not as graceful, but it works. 
 
